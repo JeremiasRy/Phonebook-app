898771029a23933b530fadc0a8ae563f1c7ce2a6
JeremiasRy <98033704+JeremiasRy@users.noreply.github.com>
2022-03-12 11:23:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const Contact = require('./models/mongoose.js')
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 
 morgan.token('postContent', function (req, res) { 
     let x = JSON.stringify(req.body)
@@ -15,97 +12,84 @@ morgan.token('postContent', function (req, res) {
     } else return null
     })
 
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postContent'))
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postContent'))
+let genId = () => {
+    let id = Math.floor(Math.random() * 1000)
+    return id
+}
+
+let contacts = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": 1
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": 3
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ]
 
 app.get('/', (req, res) => {
     res.send('<h1>Täh</h1>')
 })
 
 app.get('/api/contacts', (req, res) => {
-    Contact.find({}).then(contacts => {
-        res.json(contacts)
-    })
+    res.json(contacts)
 })
 
 app.get('/info', (req, res) => {
-    Contact.find({})
-    .then(contact => {
-        console.log(contact)
-        let date = new Date()
-        res.send(`<p>The phonebook has ${contact.length} contacts</p><br><p>${date}</p>`)
-    })
+    let bookLength = contacts.length
+    let date = new Date()
+    res.send(`<p>The phonebook has ${bookLength} contacts</p><br><p>${date}</p>`)
 })
 
-app.get('/api/contacts/:id', (req, res, next) => {
-    Contact.findById(req.params.id)
-    .then(contact => {
-        if (contact) {
-            res.json(contact)
-        } else {
-            res.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+app.get('/api/contacts/:id', (req, res) => {
+    let id = Number(req.params.id)
+    let contact = contacts.find(contact => contact.id === id)
+    if (contact) {
+        res.send(contact)
+    } else {
+        res.status(404).end()
+    }
 })
 
-app.delete('/api/contacts/:id', (req, res, next) => {
-    Contact.findByIdAndRemove(req.params.id)
-    .then(response => {
-        res.status(204).end()
-    })
-    .catch(error => next(error))
+app.delete('/api/contacts/:id', (req, res) => {
+    let id = Number(req.params.id)
+    contacts = contacts.filter(contact => contact.id !== id)
+
+    res.status(204).end()
 })
 
-
-app.post('/api/contacts', (req, res, next) => {
+app.post('/api/contacts', (req, res) => {
     let body = req.body
-    let newContact = new Contact ({
+    if (!body.name || !body.number) {
+       return res.status(400).json({
+            error: 'contact missing name or number'
+        }).end()
+    }
+    let newContact = {
         name: body.name,
         number: body.number,
-    })
-    newContact.save().then(result => {
-        res.json(newContact)
-    })
-    .catch(error => next(error))
+        id: genId()
+    }
+    contacts = contacts.concat(newContact)
+    res.json(newContact)
 })
 
-app.put('/api/contacts/:id', (req, res, next) => {
-    let body = req.body
-    let upContact = {
-        name: body.name,
-        number: body.number
-    }
-    Contact.findByIdAndUpdate(req.params.id, upContact) 
-    .then(upContact => {
-        res.json(upContact)
-    })
-    .catch(error => next(error))
-})
-
-const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(unknownEndpoint)
-  
-
-const errorHandler = (error, req, res, next) => {
-    console.log(error.message)
-  
-    if (error.name === 'CastError') {
-      return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
-  
-    next(error)
-  }
-  
-  app.use(errorHandler)
-
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
